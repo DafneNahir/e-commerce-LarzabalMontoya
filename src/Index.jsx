23e15a03cb3f70f8590f447e093780509aa88393
DafneNahir <dlm_28@hotmail.com>
2025-12-02 20:28:32 -0300

    LM

--- src/Index.jsx
+++ src/Index.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='e/commerce-LarzabalMontoya'>
+    <BrowserRouter basename='/e-commerce-LarzabalMontoya'>
       <App />
     </BrowserRouter>
   </StrictMode>
